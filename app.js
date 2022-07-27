1a60b0fba2fd85db11c1c67c1ddc80b11d09f6c7
jvsierras <78568208+jvsierras@users.noreply.github.com>
2022-07-26 20:01:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const img_url = "https://image.tmdb.org/t/p/original";
 
 // requests for movies data 
 const requests = {
-  fetchPopular: `${base_url}/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&${api}`,
-  fetchTrending: `${base_url}/trending/all/week?${api}&language=en-US`,
+  fetchPopular: `${base_url}/discover/movie?certification_country=MX&certification.lte=G&sort_by=popularity.desc&${api}`,
+  fetchTrending: `${base_url}/trending/all/week?${api}&language=es-MX`,
   fetchNetflixOrignals: `${base_url}/discover/tv?${api}&with_networks=213`,
   fetchActionMovies: `${base_url}/discover/movie?${api}&with_genres=28`,
   fetchComedyMovies: `${base_url}/discover/movie?${api}&with_genres=35`,
